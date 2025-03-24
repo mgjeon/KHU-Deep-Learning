9cecb4e596e8e81fdea38cfa790e892943d28048
mgjeon <mgjeon@khu.ac.kr>
2025-03-24 12:55:18 +0900

    edit for local

--- 07_MLP_eval.py
+++ 07_MLP_eval.py
@@ -1,8 +1,3 @@
-import torch 
-if torch.cuda.device_count() == 4:
-    import os
-    os.environ["CUDA_VISIBLE_DEVICES"] = "3"
-
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
